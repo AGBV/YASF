befebf448baae91659896814a6d3aba65d3884e3
arunoruto <21687187+arunoruto@users.noreply.github.com>
2024-02-10 03:00:15 +0100

    Testing ref idx with fe querry data

--- tests/test_url_handlers.py
+++ tests/test_url_handlers.py
@@ -1,10 +1,13 @@
 import unittest
-import glob
-import re
+# import glob
+# import re
 import sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parents[1]))
+
+import pandas as pd
+from itertools import takewhile
 
 from yasfpy.functions.material_handler import material_handler
 
@@ -17,6 +20,19 @@ class TestHandlers(unittest.TestCase):
     #     cls.relative_precision = 2e-3
 
     def test_refractiveindex_info_fe_querry(self):
-        url = "https://refractiveindex.info/database/data-nk/main/Fe/Querry.yml"
-        data = material_handler(url)
-        print(data)
+        csv_file = "tests/data/fe_querry.csv"
+        csv_data = pd.read_csv(csv_file, sep="\s+", comment="#")
+
+        header = None
+        with open(csv_file, 'r') as fobj:
+            # takewhile returns an iterator over all the lines
+            # that start with the comment string
+            header = takewhile(lambda s: s.startswith('#'), fobj)
+            header = [s.strip("# \n").split(": ") for s in header]
+            header = {s[0]: s[1].strip() for s in header}
+        self.assertTrue(header is not None)
+
+        url = header['data']
+        url_data = material_handler(url)
+
+        pd.testing.assert_frame_equal(csv_data, url_data['ref_idx'])
