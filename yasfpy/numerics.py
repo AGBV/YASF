250e034857825449c45bd3f3e86899ddb81002fd
aetherspritee <schautendustin@gmail.com>
2024-04-10 13:12:38 +0200

    changes from merge

--- yasfpy/numerics.py
+++ yasfpy/numerics.py
@@ -158,13 +158,10 @@ class Numerics:
         """
         self.__plm_coefficients()
 
-    def check_for_translation_table(self, directory: Path):
-        # check if data directory exists, else create it
-        print("::::")
+    def load_translation_table(self):
         dpath = Path(f"{files(__package__) / 'data'}")
         if not os.path.exists(dpath):
             os.makedirs(dpath)
-        # this is supposed to be used to load the data
 
         if os.path.isfile(dpath / f"lmax{self.lmax}.pickle"):
             data_raw = dpath.joinpath(Path(f'lmax{self.lmax}.pickle')).read_bytes()
@@ -180,25 +177,6 @@ class Numerics:
                 pickle.dump(res,f)
             print("Calculated translation table!")
 
-        # if os.path.isfile(directory / f"lmax{self.lmax}.pickle"):
-        #     with open(directory / f"lmax{self.lmax}.pickle", "rb") as f:
-        #         data = pickle.load(f)
-        #     self.translation_ab5 = data["wig"]
-        #     print("Found translation table and loaded it!")
-        # elif os.path.isfile(directory / f"lmax{self.lmax}.dat"):
-        #     f_data = [i.strip().strip("i").replace("E","e").split() for i in open(directory / f"lmax{self.lmax}.dat").readlines()]
-        #     dshape = tuple([int(f_data[0][0]), int(f_data[0][1]), int(f_data[1][0])])
-        #     f1 = [complex(float(i[0]),float(i[1])) for i in f_data[2:]]
-        #     self.translation_ab5 = np.array(f1).reshape(dshape)
-        #     print("Found translation table and loaded it!")
-        # else:
-        #     print(f"Didnt find translation table in specified directory: {directory}")
-        #     print("Starting calculation")
-        #     self.compute_translation_table()
-        #     res = {"wig": self.translation_ab5}
-        #     with open(f"lmax{self.lmax}", "wb") as f:
-        #         pickle.dump(res,f)
-        #     print("Calculated translation table!")
 
     def compute_translation_table(self):
         """
