a29ce912bdbb17532b1bdb211706fba624a21c11
aetherspritee <schautendustin@gmail.com>
2024-05-20 13:26:46 +0200

    cross section fix as done in master

--- yasfpy/functions/cuda_numba.py
+++ yasfpy/functions/cuda_numba.py
@@ -119,8 +119,8 @@ def compute_scattering_cross_section_gpu(
     )
 
     # atomic.add performs the += operation in sync
-    cuda.atomic.add(c_sca_real[j1], w, p_dependent.real)
-    cuda.atomic.add(c_sca_imag[j1], w, p_dependent.imag)
+    cuda.atomic.add(c_sca_real, (j1,w), p_dependent.real)
+    cuda.atomic.add(c_sca_imag, (j1,w), p_dependent.imag)
 
 
 @cuda.jit(fastmath=True)
